e087b6c118abadf5d10f211c6b61a63997e65aa6
bilis <diefaust@gmail.com>
2015-07-09 08:26:19 +0800

    Ugly hack, but this increases boot rate for mset 6.x to 100%. Take note that all mset entry points are affected, tested on 4.x and 6.1.

--- source/memchunkhax.c
+++ source/memchunkhax.c
@@ -74,10 +74,18 @@ void memchunk_arm11hax(void (*func)())
 
     // Tricks to clear the instruction cache
 #if defined(ENTRY_MSET)
+    uint8_t *const top_left_fb = (uint8_t *const)0x14184E60;
+    uint8_t *const top_right_fb = (uint8_t *const)0x14282160;
+    for(int i = 0; i < 0x00046500; ++i)
+    {
+        top_left_fb[i] = 0xFF;
+        top_right_fb[i] = 0xFF;
+    }
+
     int slide_len = 0x1000;
 
     // Build a NOP slide
-    build_nop_slide(arm11_buffer, 0x1000);
+    build_nop_slide(arm11_buffer, slide_len);
 
     // Copy the NOP slide
     gspwn_copy((void *)(0x14000000 + APP_CODE_OFFSET + 0x4000), arm11_buffer,
