14d726d6e5746aff3609cc996ea14115c1ffe0da
173210 <root.3.173210@live.com>
2015-07-11 15:20:57 +0900

    License under GNU GPLv2.0

--- Readme.md
+++ Readme.md
@@ -59,3 +59,17 @@ Thanks
 * smea and Yellows8 for gspwn
 * Apache Thunder for being a fearless test-kanninchen.
 * Gateway for some offsets
+
+License
+-------
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 2.0
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
--- source/payload/fatfs/sdmmc/sdmmc.c
+++ source/payload/fatfs/sdmmc/sdmmc.c
@@ -1,6 +1,23 @@
-// Copyright 2014 Normmatt
-// Licensed under GPLv2 or any later version
-// Refer to the license.txt file included.
+/*
+ * Copyright (C) 2015 mid-kid
+ *
+ * This file is part of CakeHax.
+ *
+ * CakeHax is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2.0
+ *
+ * CakeHax is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CakeHax.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * This file was originally licensed under GPLv2 or
+ * any later version by Normmatt.
+ */
 
 #include "common.h"
 
